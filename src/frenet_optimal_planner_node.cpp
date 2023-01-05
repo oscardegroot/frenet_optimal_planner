df0ef8058ee673d8ed27abc0022d0583d9e588be
oscardegroot <groot.oscar.de@gmail.com>
2023-01-05 10:52:38 +0100

    added dvc and modifications to plot the road limits.

--- src/frenet_optimal_planner_node.cpp
+++ src/frenet_optimal_planner_node.cpp
@@ -335,32 +335,32 @@ namespace fop
     }
 
     // left boundaries
-  for (size_t i = 0; i < lane_.points.size() - 2; i++)
-  {
-    geometry_msgs::Point p;
-    p.x = lane_.points[i].point.x + SETTINGS.curr_lane_width/2;
-    p.y = lane_.points[i].point.y + SETTINGS.curr_lane_width/2;
-    p.z = -0.5e-3;
+    for (size_t i = 0; i < lane_.points.size() - 2; i++)
+    {
+      geometry_msgs::Point p;
+      p.x = lane_.points[i].point.x;
+      p.y = lane_.points[i].point.y + LANE_WIDTH / 2;
+      p.z = -0.5e-3;
 
-    if (i > 0)
-      line.addLine(prev_point, p);
-    
-    prev_point = p;
-  }
+      if (i > 0)
+        line.addLine(prev_point, p);
 
-  // right boundaries
-  for (size_t i = 0; i < lane_.points.size() - 2; i++)
-  {
-    geometry_msgs::Point p;
-    p.x = lane_.points[i].point.x - SETTINGS.curr_lane_width/2;
-    p.y = lane_.points[i].point.y - SETTINGS.curr_lane_width/2;
-    p.z = -0.5e-3;
+      prev_point = p;
+    }
 
-    if (i > 0)
-      line.addLine(prev_point, p);
-    
-    prev_point = p;
-  }
+    // right boundaries
+    for (size_t i = 0; i < lane_.points.size() - 2; i++)
+    {
+      geometry_msgs::Point p;
+      p.x = lane_.points[i].point.x;
+      p.y = lane_.points[i].point.y - LANE_WIDTH / 2;
+      p.z = -0.5e-3;
+
+      if (i > 0)
+        line.addLine(prev_point, p);
+
+      prev_point = p;
+    }
 
     ros_markers_reference_path_->publish();
     publishRefSpline(ref_spline_);
